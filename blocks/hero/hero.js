5ede396415de348f8b763b72dcba7ce60fe3ca7a
Luca Nerlich <luca.nerlich@gmail.com>
2025-06-17 13:47:49 +0200

    start with video hero

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,38 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+function createVideoHero(block) {
+
+    // wip, merge with code in cards
+
+   const posterImage = block.querySelector('picture > img');
+    const optimizedPicture = createOptimizedPicture(posterImage.src, posterImage.alt, false, [{ width: '750' }]);
+    const optimizedImg = optimizedPicture.querySelector('img');
+
+    const videoContainer = document.createElement('div');
+    videoContainer.className = 'cards-video-container';
+
+    const video = document.createElement('video');
+    video.setAttribute('controls', '');
+    video.setAttribute('poster', optimizedImg.src);
+    video.setAttribute('preload', 'metadata');
+
+    const source = document.createElement('source');
+    // source.src = videoUrl;
+    // source.type = getVideoMimeType(videoUrl);
+
+    video.appendChild(source);
+    videoContainer.appendChild(video);
+
+    // Replace the image div with video container
+    block.append(videoContainer);
+    console.log('posterImage', posterImage);
+    return Promise.resolve(undefined);
+}
+
 export default async function decorate(block) {
+    const isVideo = block.classList.contains('video');
+    if (isVideo) return createVideoHero(block);
+
     const h1 = block.querySelector('h1');
     if (!h1) return;
 
